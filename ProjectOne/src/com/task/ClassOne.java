0678ba46e87ce4cf55c5ebc1408e45f57692e480
k.Gayatri <k.Gayatri@sureshbabu>
2022-10-04 15:04:59 +0530

    second commit

--- ProjectOne/src/com/task/ClassOne.java
+++ ProjectOne/src/com/task/ClassOne.java
@@ -1,9 +1,41 @@
 package com.task;
 
-public class ClassOne {
+import java.io.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ClassOne 
+{
+
+	public static void main(String[] args) 
+	{
+				
+		int rno = 0;
+		String name = null;
+		double avg = 0;
+		
+		BufferedReader br = new BufferedReader(
+				      new InputStreamReader(System.in));
+		try
+		{
+		System.out.println("Enter your roll num");
+		rno = Integer.parseInt(br.readLine());
+		
+		System.out.println("Enter your name");
+		name = br.readLine();
+		
+		System.out.println("Enter your avg marks");
+		avg = Double.parseDouble(br.readLine());
+		
+		}
+		catch(IOException ie)
+		{
+			System.out.println(ie);
+		}
+		
+		System.out.println("Your data is");
+		System.out.println("Rno = " + rno);
+		System.out.println("Name = " + name);
+		System.out.println("Avg = " + avg);
+		
 
 	}
 
